6198ba72f0f8d594d8f88997faef33ff975e30d1
larsenamy14 <larsenamy14@gmail.com>
2024-02-12 18:33:49 -0800

    Uploaded hw

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,51 @@
+# Import the dependencies.
+from flask import Flask, jsonify
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save references to each table
+Station = Base.classes.station
+Measurement = Base.classes.measurement
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def home():
+    """List all the available routes."""
+    return (
+        f"Welcome to the Precipitation Page!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
